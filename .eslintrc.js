a5591b218e6c12cf6e5b31a85d65a1d2cf4ea64e
Denis Gukov <denis.guk@konomic.com>
2025-02-28 23:56:26 +0300

    chatgpt: added generate endpoint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,48 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: './tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint', 'prettier', 'react', 'react-hooks', '@next/eslint-plugin-next', 'prisma'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@next/next/recommended',
   ],
   root: true,
   env: {
     node: true,
     jest: true,
+    browser: true,
+    es6: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'node_modules/', 'dist/'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    // TypeScript
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+
+    // React
+    'react/jsx-uses-react': 'off', // Не нужен с React 17+
+    'react/react-in-jsx-scope': 'off', // Не нужен с React 17+
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+
+    // Prettier
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+
+    // Prisma
+    'prisma/no-unchecked-queries': 'warn',
+    'prisma/require-indexes': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 };
